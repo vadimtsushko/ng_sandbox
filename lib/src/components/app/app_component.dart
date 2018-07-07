f23c31a400dc3abf82bc3c6a932baf4d6d1fd945
mk <mou.mail.com@gmail.com>
2018-07-07 13:52:32 +0300

    add top text

--- lib/src/components/app/app_component.dart
+++ lib/src/components/app/app_component.dart
@@ -2,6 +2,7 @@ import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_tour_of_heroes/src/routes/routes.dart';
 @Component(
+
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
@@ -10,7 +11,10 @@ import 'package:angular_tour_of_heroes/src/routes/routes.dart';
 )
 class AppComponent
 {
+
   final Routes routes;
   AppComponent(this.routes);
 
 }
+
+
