093a76816de5279990b8441a3dff36b7fa669b95
stekolschikov <stekolschikov@mail.ua>
2018-07-30 12:54:48 +0500

    refactoring

---
+++ lib/src/app_controller/filter_selector.dart
@@ -0,0 +1,91 @@
+class AvailException implements Exception {
+  final String msg;
+
+  const AvailException([this.msg]);
+
+  @override
+  String toString() => msg ?? 'AvailException';
+}
+
+class FilterSelector {
+
+  List<String> headers;
+  List<List> dataIn;
+  List<List> dataOut;
+  List<List> filter;
+  List<String> operators = ['<', '>', '<=', '>=', '=='];
+
+  init({List<String> headers = testHeaders, List<List> data = testData, List<List> filter = testFilter}) {
+    this.headers = List.from(headers);
+    this.dataIn = List.from(data);
+    this.filter = List.from(filter);
+    filterOut();
+  }
+
+  bool isCanAdd(int valData,String operator,int filterData) {
+    var res = false;
+    if (operator == '<') {
+      res = valData < filterData;
+    } else if (operator == '>') {
+      res = valData > filterData;
+    } else if (operator == '<=') {
+      res = valData <= filterData;
+    } else if (operator == '>=') {
+      res = valData >= filterData;
+    } else if (operator == '==') {
+      res = (valData == filterData);
+    }
+    return res;
+  }
+
+  void rmOneFilter(List text){
+    filter.remove(text);
+    filterOut();
+  }
+
+  void reset() {
+    filter = [];
+    filterOut();
+  }
+
+  void filterOut() {
+    List<List> out = [];
+    for (int i = 0; i < dataIn.length; i++) {
+      var isAdd = true;
+      for (int j = 0; j < filter.length; j++) {
+        int position = _getColPosition(filter[j][0]);
+        int valData = dataIn[i][position];
+        var valOpr = filter[j][1];
+        int filterData = filter[j][2];
+        isAdd = isCanAdd(valData, valOpr, filterData);
+      }
+      if (isAdd) {
+        out.add(dataIn[i]);
+      }
+    }
+    dataOut = out;
+  }
+
+  int _getColPosition(String colText) {
+    var position = 0;
+    for (int h = 0; h < headers.length; h++) {
+      if (headers[h] == colText) {
+        position = h;
+      }
+    }
+    return position;
+  }
+}
+
+
+const List<String> testHeaders = ['Сумма продаж тыс.руб', 'Наценка, тыс. руб', 'Остатки, тыс руб', 'Количество продано, шт'];
+const List<List> testData = [
+  [1, 4, 1, 0],
+  [2, 3, 2,  1],
+  [3, 2, 3, 4],
+  [4, 1, 4, 10]
+];
+const List<List> testFilter = [
+  ['col1', '==', 3],
+//  ['col1', '>', 2],
+];
--- lib/src/components/filter/filter.dart
+++ lib/src/components/filter/filter.dart
@@ -3,6 +3,9 @@ import 'package:angular_router/angular_router.dart';
 import 'dart:async';
 import 'dart:html';
 
+import 'package:angular_tour_of_heroes/src/app_controller/filter_selector.dart';
+
+
 @Component(
   selector: 'my-filter',
   templateUrl: 'filter.html',
@@ -11,110 +14,111 @@ import 'dart:html';
 )
 
 class FilterComponent implements OnInit, OnChanges {
-  List<String> headers;
-  List<List> dataIn;
-  List<List> dataOut;
-  List<List> filter;
-  List<String> operators = ['<', '>', '<=', '>=', '=='];
+//  List<String> headers;
+//  List<List> dataIn;
+//  List<List> dataOut;
+//  List<List> filter;
+//  List<String> operators = ['<', '>', '<=', '>=', '=='];
 
   Future<void> ngOnInit() async {
-    this.init();
+//    this.init();
   }
 //TODO: Почему не отрабатывает?
   Future<void> ngOnChanges(Map<String, SimpleChange> changes) async {
     print('ngOnChanges');
   }
 
-  init({headers = testHeaders, data = testData, filter = testFilter}) {
-    this.headers = List.from(headers);
-    this.dataIn = List.from(data);
-    this.filter = List.from(filter);
-    filterOut();
-  }
+//  init({headers = testHeaders, data = testData, filter = testFilter}) {
+//    this.headers = List.from(headers);
+//    this.dataIn = List.from(data);
+//    this.filter = List.from(filter);
+//    filterOut();
+//  }
 
-  void reset() {
-    filter = [];
-    filterOut();
-  }
+//  void reset() {
+//    filter = [];
+//    filterOut();
+//  }
 
-  void add() {
-    var columnText = _getColumnText();
-    var operatorsText = _getOperatorsText();
-    var valueText = _getValueText();
-    if (valueText.length > 0) {
-      filter.add([columnText, operatorsText, int.parse(valueText)]);
-      filterOut();
-    }
-  }
+//  void add() {
+//    var columnText = getColumnText();
+//    var operatorsText = getOperatorsText();
+//    var valueText = getValueText();
+//    if (valueText.length > 0) {
+//      filter.add([columnText, operatorsText, int.parse(valueText)]);
+//      filterOut();
+//    }
+//  }
 
-  String _getColumnText() {
+  FilterSelector FS = new FilterSelector()..init();
+
+  String getColumnText() {
     var column = (querySelector('#column') as SelectElement);
     return column.value;
   }
 
-  String _getOperatorsText() {
+  String getOperatorsText() {
     var operators = (querySelector('#operators') as SelectElement);
     return operators.value;
   }
 
-  String _getValueText() {
+  String getValueText() {
     var value = (querySelector('#value') as InputElement);
     return value.value;
   }
-
-  filterOut() {
-    List<List> out = [];
-    for (int i = 0; i < dataIn.length; i++) {
-      var isAdd = true;
-      for (int j = 0; j < filter.length; j++) {
-        int position = _getColPosition(filter[j][0]);
-//        int position = 0;
-        int valData = dataIn[i][position];
-        var valOpr = filter[j][1];
-        int filterData = filter[j][2];
-        isAdd = isCanAdd(valData, valOpr, filterData);
-      }
-      if (isAdd) {
-        out.add(dataIn[i]);
-      }
+  void add() {
+    var columnText = getColumnText();
+    var operatorsText = getOperatorsText();
+    var valueText = getValueText();
+    if (valueText.length > 0) {
+      FS.filter.add([columnText, operatorsText, int.parse(valueText)]);
+      FS.filterOut();
     }
-    dataOut = out;
   }
+//  rmOneFilter(List text){
+//    print(text[0]);
+//    filter.remove(text);
+//    filterOut();
+//  }
 
-  int _getColPosition(colText) {
-    var position = 0;
-    for (int h = 0; h < headers.length; h++) {
-      if (headers[h] == colText) {
-        position = h;
-      }
-    }
-    return position;
-  }
+//  filterOut() {
+//    List<List> out = [];
+//    for (int i = 0; i < dataIn.length; i++) {
+//      var isAdd = true;
+//      for (int j = 0; j < filter.length; j++) {
+//        int position = _getColPosition(filter[j][0]);
+////        int position = 0;
+//        int valData = dataIn[i][position];
+//        var valOpr = filter[j][1];
+//        int filterData = filter[j][2];
+//        isAdd = isCanAdd(valData, valOpr, filterData);
+//      }
+//      if (isAdd) {
+//        out.add(dataIn[i]);
+//      }
+//    }
+//    dataOut = out;
+//  }
 
-  bool isCanAdd(int valData,String operator,int filterData) {
-    var res = false;
-    if (operator == '<') {
-      res = valData < filterData;
-    } else if (operator == '>') {
-      res = valData > filterData;
-    } else if (operator == '<=') {
-      res = valData <= filterData;
-    } else if (operator == '>=') {
-      res = valData >= filterData;
-    } else if (operator == '==') {
-      res = (valData == filterData);
-    }
-//    print("isCanAdd ${valData} ${operator} ${filterData} ${res}");
-    return res;
-  }
+//  int _getColPosition(colText) {
+//    var position = 0;
+//    for (int h = 0; h < headers.length; h++) {
+//      if (headers[h] == colText) {
+//        position = h;
+//      }
+//    }
+//    return position;
+//  }
+
+
 }
 
-const List<String> testHeaders = ['col1', 'col2', 'col3'];
+const List<String> testHeaders = ['Сумма продаж тыс.руб', 'Наценка, тыс. руб', 'Остатки, тыс руб', 'Количество продано, шт'];
 const List<List> testData = [
-  [1, 4, 1],
-  [2, 3, 2],
-  [3, 2, 3],
-  [4, 1, 4]
+  [1, 4, 1, 0],
+  [2, 3, 2,  1],
+  [3, 2, 3, 4],
+  [4, 1, 4, 10]
 ];
 const List<List> testFilter = [
   ['col1', '==', 3],
