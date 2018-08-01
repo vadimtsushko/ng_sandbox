4cbead21395796cb30f13c7a16c5395c8b399de0
stekolschikov <stekolschikov@mail.ua>
2018-08-01 17:00:35 +0500

    fix resetFMC

--- lib/src/components/filterModal/filterModal.dart
+++ lib/src/components/filterModal/filterModal.dart
@@ -2,6 +2,7 @@ import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'dart:async';
+import 'dart:html';
 
 import 'package:angular_tour_of_heroes/src/app_controller/filter_selector.dart';
 import 'package:angular_tour_of_heroes/src/components/filter/filter.dart';
@@ -16,14 +17,23 @@ import 'package:angular_tour_of_heroes/src/components/filter/filter.dart';
 
 class FilterModalComponent extends FilterComponent {
 
-  String xxx;
   void openFMC(){
-    print(xxx);
+    FS.reset();
+    print('---');
+
+    var value = (querySelector('#value') as InputElement);
+    value.value = "";
+
+    var operators = (querySelector('#operators') as SelectElement);
+    operators.value = "";
+
+    var column = (querySelector('#column') as SelectElement);
+    column.value = "";
   }
 
   void resetFMC(){
     FS.reset();
-    streamEventProcessing(new Data('reset', []));
+//    streamEventProcessing(new Data('reset', []));
   }
 
   void rmFMC(String type, String op, int val){
@@ -40,12 +50,7 @@ class FilterModalComponent extends FilterComponent {
     streamEventProcessing(new Data('apply', FS.filter));
   }
 
-  @Input()
-  Stream get streamEventProcessing_eI => _innerListController_streamEventProcessingI.stream;
-  final _innerListController_streamEventProcessingI = StreamController();
-  void streamEventProcessingI(e){
-    print(e);
-  }
+
 
   @Output()
   Stream get streamEventProcessing_e => _innerListController_streamEventProcessing.stream;
