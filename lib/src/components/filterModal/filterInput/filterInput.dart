18a9c80a75c8c21a16b98872940758048ef4f0d8
v.a.stekolschikov@gmail.com <v.a.stekolschikov@gmail.com>
2018-08-16 11:19:13 +0500

    FilterInput data type

--- lib/src/components/filterModal/filterInput/filterInput.dart
+++ lib/src/components/filterModal/filterInput/filterInput.dart
@@ -2,8 +2,13 @@ import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_forms/angular_forms.dart';
 
-import '../filterModal.dart';
 
+import 'package:ng_sandbox/src/components/filterModal/filter_selector.dart';
+
+import 'package:model/model.dart';
+
+import '../filterModal.dart';
+import '../filter_selector.dart';
 import 'package:ng_sandbox/src/app_controller/event_bus.dart';
 
 @Component(
@@ -20,15 +25,15 @@ class FilterInput {
   String operatorsMin;
   String operatorsMax;
 
-  var selectorLoc;
+  FilterSelector selectorLoc;
   @Input()
-  set selector(value) {
+  set selector(FilterSelector value) {
     selectorLoc = value;
   }
 
-  var measureLoc;
+  MeasureForFilter measureLoc;
   @Input()
-  set measure(value) {
+  set measure(MeasureForFilter value) {
     measureLoc = value;
   }
 
